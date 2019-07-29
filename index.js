75f501e9d0444eb6c53779976f9cfc7cead7afb0
zhanglb <1032989811@qq.com>
2019-07-29 15:45:54 +0800

    更改babel-core依赖路径为项目目录

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
-
-var babel = require('babel-core');
-
+var path = require('path')
+var processCWD = global.fis.processCWD
+var babel = require(path.resolve(processCWD, 'node_modules/babel-core'));
 module.exports = function (content, file, conf) {
    // 只有es.js后缀名的需要babel编译
    var reg = /\.es\.js$/, fullname = file.fullname
